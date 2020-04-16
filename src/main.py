982f22e4b82d9b10ce50be2953702b27bf3bcf0c
Christian Johnson <christian.johnson@deepsight.de>
2020-04-16 13:19:39 +0200

    Adjusted main script to output db url.

--- src/main.py
+++ src/main.py
@@ -12,10 +12,10 @@ import pandas as pd
 # Instantiate the sacred experiment.
 ex = Experiment()
 db_name = 'pnlp'
+url = f'mongodb+srv://{username}:{password}@cluster0-8ejtu.azure.mongodb.net/{db_name}?retryWrites=true&w=majority'
 
 # Add the MongoDB observer.
-# ex.observers.append(MongoObserver(url=f'mongodb+srv://{username}:{password}@cluster0-8ejtu.azure.mongodb.net/{db_name}?retryWrites=true&w=majority',
-#                                   db_name=f'{db_name}'))
+# ex.observers.append(MongoObserver(url=url, db_name=f'{db_name}'))
 
 # Configure the experiment parameters. These will be logged by sacred.
 @ex.config
@@ -71,4 +71,4 @@ def run(experimenter, data_path, data_language, preprocessing_param, topic_model
     # TODO: ex.log_scalar(custom_eval_metric)
     ex.add_artifact(clusters_path)
 
-    print('Please visit omniboard to view the experiment results. You may need to gain access to the MongoDB.')
+    print(f'Please visit omniboard to view the experiment results. You may need to gain access to the MongoDB.\n\tURL: {url}')
